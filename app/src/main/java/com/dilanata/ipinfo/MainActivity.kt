996e5e271cbbeb44db95ed1e1aa573d6a2869a1b
Dilan <dilan.ata@pinsoft.ist>
2022-09-15 01:26:41 +0300

    create project

---
+++ app/src/main/java/com/dilanata/ipinfo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dilanata.ipinfo
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.dilanata.ipinfo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
